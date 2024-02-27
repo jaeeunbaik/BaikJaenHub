b2056f948ce1504d02297ac37208f5087ff50f98
jaeeunbaik <109750369+jammaya@users.noreply.github.com>
2024-02-27 13:46:44 +0900

    [level 0] Title: 모스부호 (1), Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,12 @@
+def solution(letter):
+    letter_list = letter.split(' ')
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    for i, l in enumerate(letter_list):
+        letter_list[i] = morse[l]
+    return ''.join(letter_list)
