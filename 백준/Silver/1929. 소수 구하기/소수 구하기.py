73f44aee12f7eaff9b6c198283162f25c52a3a69
jaeeunbaik <109750369+jammaya@users.noreply.github.com>
2024-03-12 19:30:13 +0900

    [Silver III] Title: 소수 구하기, Time: 3188 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,13 @@
+M, N = list(map(int, input().split(' ')))
+
+def is_prime(num):
+    if num == 1:
+        return False
+    for a in range(2, int(num**0.5)+1):
+        if num % a == 0:
+            return False
+    return True
+
+for i in range(M, N+1):
+    if is_prime(i):
+        print(i)
