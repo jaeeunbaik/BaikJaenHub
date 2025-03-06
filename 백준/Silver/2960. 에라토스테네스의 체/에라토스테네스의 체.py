040db912f370ffb04b3f9d81f9d4c0e5cdf30fb6
jaeeunbaik <109750369+jaeeunbaik@users.noreply.github.com>
2025-03-06 23:07:49 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
@@ -1,14 +1,34 @@
-N , K = list(map(int, input().split(' ')))
+#문제
+#에라토스테네스의 체는 N보다 작거나 같은 모든 소수를 찾는 유명한 알고리즘이다.
+
+#이 알고리즘은 다음과 같다.
+
+#2부터 N까지 모든 정수를 적는다.
+#아직 지우지 않은 수 중 가장 작은 수를 찾는다. 이것을 P라고 하고, 이 수는 소수이다.
+#P를 지우고, 아직 지우지 않은 P의 배수를 크기 순서대로 지운다.
+#아직 모든 수를 지우지 않았다면, 다시 2번 단계로 간다.
+#N, K가 주어졌을 때, K번째 지우는 수를 구하는 프로그램을 작성하시오.
+
+#입력
+#첫째 줄에 N과 K가 주어진다. (1 ≤ K < N, max(1, K) < N ≤ 1000)
+
+#출력
+#첫째 줄에 K번째 지워진 수를 출력한다.
+
+N, K = map(int, input().split())
+
+num_list = [True] * (N + 1)
+num_list[0] = False
+num_list[1] = False
+cnt = 0
+
+for n in range(N + 1):
+    if num_list[n] == True:
+        for m in range(n, N+1, n):
+            if num_list[m] == False:
+                continue
+            num_list[m] = False
+            cnt += 1
+            if cnt == K:
+                print(m)
     
-#7, 3
-prime = [True] * (N+1)
-k = 0
-for i in range(2, N+1):
-    if prime[i]:
-        for j in range(i, N+1, i):
-            if prime[j]:
-                prime[j] = False
-                k += 1
-                if int(k) == int(K):
-                    print(j)
-                    break
