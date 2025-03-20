a5ec9da0332ec2ffbfaf466045280b33702ba253
jaeeunbaik <109750369+jaeeunbaik@users.noreply.github.com>
2025-03-21 00:33:18 +0900

    [level 3] Title: 입국심사, Time: 667.30 ms, Memory: 13.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,16 @@
+def solution(n, times):
+    answer = 0
+    times.sort()
+    left , right = times[0], times[0] * n
+    while left < right:
+        mid = (left + right) // 2
+        total = 0
+        for t in times:
+            total += mid // t
+        if total >= n:
+            right = mid
+        elif total < n:
+            left = mid
+        if left == right - 1:
+            break
+    return right
